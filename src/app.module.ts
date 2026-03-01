4f67185df201b7221895e345fb827165dacd310b
Alvaro Navarro <quinchonavarro@gmail.com>
2026-03-01 11:01:45 -0400

    change db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,32 +6,28 @@ import { VacunasModule } from './vacunas/vacunas.module';
 import { EmployeesModule } from './employees/employees.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get<string>('POSTGRES_HOST'),
-        port: parseInt(configService.get<string>('POSTGRES_PORT') || '5432'),
-        username: configService.get<string>('POSTGRES_USERNAME'),
-        password: configService.get<string>('POSTGRES_PASSWORD'),
-        database: configService.get<string>('POSTGRES_DATABASE'),
-        autoLoadEntities: true,
-        synchronize: true,
-        ssl: configService.get<boolean>('POSTGRES_SSL') === true,
-        extra: {
-          ssl: configService.get<boolean>('POSTGRES_SSL') === true ? {
-            rejectUnauthorized: false
-          } : null
-        }
-      }),
-      inject: [ConfigService],
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USERNAME,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
+      autoLoadEntities: true,
+      synchronize: true,
+      ssl: process.env.POSTGRES_SSL === 'true',
+      extra: {
+        ssl: process.env.POSTGRES_SSL === 'true' ? {
+          rejectUnauthorized: false
+        } : null
+      }
     }),
     GatosModule,
     RazasModule,
